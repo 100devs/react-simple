f9ab65fa9d556b7841c8d6b7d37d69132fa3b1a4
JessieTMossa <76855680+JessieTMossa@users.noreply.github.com>
2021-06-03 19:32:39 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+// import react for this project
 import React, { Component } from 'react';
+// import app.css file from components folder on line 2
 import './App.css';
+// import Header file from components folder on line 2
 import Header from './Header';
+// import SectionMain file from components folder on line 2
 import SectionMain from './SectionMain';
+// import Aside file from components folder on line 2
 import Aside from './Aside';
+// import Footer file from components folder on line 2
 import Footer from './Footer';
 
+// create class-based component
 class App extends Component {
-
+// render method
   render() {
+//    return rendered HTML of the individual sections
     return (
       <div className="App">
         <Header backColor="green" width="50%"></Header>
@@ -18,5 +26,5 @@ class App extends Component {
     );
   }
 }
-
+// export App so it can be accessed in index.js
 export default App;
