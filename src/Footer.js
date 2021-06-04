36ab618234dd980166165825d281188b8077c74e
JimboC <>
2021-06-04 00:17:42 +0000

    commented all files in project

--- src/Footer.js
+++ src/Footer.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './Footer.css';
 
+// class-based component that controls how the Footer behaves
 class Footer extends Component {
   render() {
     return (
         <footer className="Footer">
-        </footer>
-
+        </footer>0
     );
+    // tells what to return when you plug Footer into App.js
   }
 }
 
 export default Footer;
+// allow access of Footer outside this file
--- src/SectionMain.js
+++ src/SectionMain.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './SectionMain.css';
 
+
+//class based component for SectionMain - holds all of our SectionMain's components
 class SectionMain extends Component {
   render() {
     return (
         <section className="SectionMain">
         </section>
-
     );
+    // this tells our App.js what to render when we plug SectionMain component in
   }
 }
 
 export default SectionMain;
+// allow access of SectionMain outside this file
