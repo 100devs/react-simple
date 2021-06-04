3b03bdda44f1309dd9c5040d1cb071a5388ba7f0
khafrewatkins <khafrewatkins@gmail.com>
2021-06-03 19:16:35 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-
+// tells the DOM what to render - App.js renders to the root div of index.html
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
