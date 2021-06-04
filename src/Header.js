77b141c04a1bd6d535655209d6cedb7ba6684d93
JessieTMossa <76855680+JessieTMossa@users.noreply.github.com>
2021-06-03 20:06:23 -0500

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,14 @@
+//  import react and component for use in this file
 import React, { Component } from 'react';
+// import Header.css file
 import './Header.css';
 
+// establish class-based component for Header element
 class Header extends Component {
+//   render style variable and return header with styling
   render() {
     const style = {
+//  grab styling from App.js (defined in return)
       width: this.props.width,
       backgroundColor: this.props.backColor
     }
@@ -15,6 +20,7 @@ class Header extends Component {
   }
 }
 
+// export Header component
 export default Header;
 
 
