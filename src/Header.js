344669aeb34c350473b88cc6ec5926a9441ed9a3
Jonathan Yau <jonathankyau@gmail.com>
2021-06-07 14:16:44 -0700

    added comments

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import './Header.css';
+import React, { Component } from 'react'; // import react and component
+import './Header.css'; //import styling
 
-class Header extends Component {
+class Header extends Component { //create Header object from Component
   render() {
-    const style = {
+    const style = { //create style properties on Header class
       width: this.props.width,
       backgroundColor: this.props.backColor
     }
-    return (
+    return ( //return Header with object style and className Header-main
         <header style={style} className="Header-main">
         </header>
 
@@ -15,7 +15,7 @@ class Header extends Component {
   }
 }
 
-export default Header;
+export default Header; // export Header component
 
 
 //document.querySelector("header").style.backgroundColor = "red"
