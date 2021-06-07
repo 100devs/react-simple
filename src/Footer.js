056ff8cbc1697a680c08f3d863a04e43313c393d
Jonathan Yau <jonathankyau@gmail.com>
2021-06-07 14:14:01 -0700

    added comments

--- src/Footer.js
+++ src/Footer.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import './Footer.css';
+import React, { Component } from 'react'; // import react and component
+import './Footer.css'; //import css styling
 
-class Footer extends Component {
+class Footer extends Component { // creating Footer object of Component
   render() {
     return (
-        <footer className="Footer">
+        <footer className="Footer"> //give className to footer component
         </footer>
 
     );
   }
 }
 
-export default Footer;
+export default Footer; //export Footer component
