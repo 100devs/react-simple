ebe9c5301240d5efd7e70ffa7b5455189ab2f7a1
Jonathan Yau <jonathankyau@gmail.com>
2021-06-07 14:27:13 -0700

    added comments

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'; //import react
+import ReactDOM from 'react-dom'; //importReactDOM
+import './index.css'; //import css
+import App from './App'; //import App
+import registerServiceWorker from './registerServiceWorker'; //import registerServiceWorker for measuring performance
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root')); //render App component in DOM where div with 'root' class
+registerServiceWorker(); //invoke performance measure
