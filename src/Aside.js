b7a951b7444eae0bda514d6dd73ce026fc02b216
JessieTMossa <76855680+JessieTMossa@users.noreply.github.com>
2021-06-09 18:21:00 -0500

    Update Aside.js

--- src/Aside.js
+++ src/Aside.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 // import Aside styling file from src
 import './Aside.css';
 
-// create class-based component
+// create class-based component for the aside section
 class Aside extends Component {
   // render method
   render() {
-    //    return rendered HTML of the footer
+    //    return rendered HTML of the aside section
     return (
       //   aside has class name so we can apply CSS styling to it, camelCase indicates JSX
         <aside className="Aside">
