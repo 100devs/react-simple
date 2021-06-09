0abb6e6433c22e6645b7142ef7a1caf7a4c7a046
JessieTMossa <76855680+JessieTMossa@users.noreply.github.com>
2021-06-09 18:21:21 -0500

    Update SectionMain.js

--- src/SectionMain.js
+++ src/SectionMain.js
@@ -1,9 +1,15 @@
+// import react for this project
 import React, { Component } from 'react';
+// import SectionMain styling file from src
 import './SectionMain.css';
 
+// create class-based component for main section
 class SectionMain extends Component {
+  // render method
   render() {
+    //    return rendered HTML of the main section
     return (
+      //   section has class name so we can apply CSS styling to it, camelCase indicates JSX
         <section className="SectionMain">
         </section>
 
@@ -11,4 +17,5 @@ class SectionMain extends Component {
   }
 }
 
+// export SectionMain so it can be imported to App (parent component)
 export default SectionMain;
